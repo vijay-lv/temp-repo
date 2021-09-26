a20bccefceed148baf4a754879f9ff6f453115f1
WELCOME <vjvictory.90@gmail.com>
2021-09-26 10:57:43 -0700

    dfgh

---
+++ 1-node/app.js
@@ -0,0 +1,17 @@
+const http=require('http')
+
+const fs=require('fs')
+
+http.createServer((req,res)=>{
+
+    const file=fs.createReadStream('./content/sub/bigfile2.txt','utf8');
+
+    file.on('open',()=>{
+
+        file.pipe(res)
+    })
+    file.on('err',(err)=>{
+
+        res.end(err)
+    })
+}).listen(5000)
